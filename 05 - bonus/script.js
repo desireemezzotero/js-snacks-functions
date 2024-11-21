ed1a79a2d8bb274a2fe0559157cc2bc61aaa8fd7
desireemezzotero <desireemezzotero1@gmail.com>
2024-11-21 18:20:59 +0100

    [add] exercise five

--- 05 - bonus/script.js
+++ 05 - bonus/script.js
@@ -6,13 +6,29 @@ e buonasera se è sera (oltre le 17)
 */
 
 const name = 'Mario';
+const date = new Date ();
+/* console.log(date.getHours()) */
 
 
 // Dichiara la funzione qui.
 
+function array (names) { 
+  names = '';
+  if (date < 13) {
+    names = 'buongiorno'
+  } else if (date > 13 && date < 17) {
+     names = 'buon pomeriggio'
+  } else {
+    names = 'buonasera'
+  }
+
+  return names
+}
+ 
+
 
 // Invoca la funzione qui e stampa il risultato in console
-
+ console.log(array(name), name)
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
